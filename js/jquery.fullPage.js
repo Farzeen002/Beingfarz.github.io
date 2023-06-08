d15668e4de79922abf323aa01a34277cdc5d7071
Farzeen Ahmed <farzeenahmed2000@gmail.com>
2023-06-08 14:12:41 +0530

    version --4

--- js/jquery.fullPage.js
+++ js/jquery.fullPage.js
@@ -1996,18 +1996,20 @@
         * https://github.com/alvarotrigo/fullPage.js/issues/194#issuecomment-34069854
         */
         function getEventsPage(e){
-            var events = [];
-
-            events.y = (typeof e.pageY !== 'undefined' && (e.pageY || e.pageX) ? e.pageY : e.touches[0].pageY);
-            events.x = (typeof e.pageX !== 'undefined' && (e.pageY || e.pageX) ? e.pageX : e.touches[0].pageX);
-
-            //in touch devices with scrollBar:true, e.pageY is detected, but we have to deal with touch events. #1008
-            if(isTouch && isReallyTouch(e)){
+            var events = {
+                y: 0,
+                x: 0
+              };
+            
+              if (typeof e.pageY !== 'undefined' && (e.pageY || e.pageX)) {
+                events.y = e.pageY;
+                events.x = e.pageX;
+              } else if (typeof e.touches !== 'undefined' && e.touches[0]) {
                 events.y = e.touches[0].pageY;
                 events.x = e.touches[0].pageX;
-            }
-
-            return events;
+              }
+            
+              return events;
         }
 
         function silentLandscapeScroll(activeSlide){
